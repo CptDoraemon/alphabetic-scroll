b17ff0c3a48321a8a64991cd39d2200a2471b859
Xiaoxi <yxx0827@gmail.com>
2020-11-04 15:50:59 -0500

    improved binary search

--- src/alphabetic-scroll/alphabetic-scroll.jsx
+++ src/alphabetic-scroll/alphabetic-scroll.jsx
@@ -34,19 +34,20 @@ const useBindAnchorToScroll = (anchorList, anchorRefs, setAnchor, isTouchingRef)
 
   useEffect(() => {
     const search = (nums, target) => {
-      let lo = 0, hi = nums.length;
-      while (lo < hi) {
-        const mid = Math.floor((lo+hi)/2);
+      let lo = 0, hi = nums.length - 1;
+      while(lo < hi) {
+        const mid = Math.floor((lo + hi) / 2);
         if (nums[mid] === target) {
-          return mid;
-        }
-        if (nums[mid] <= target) {
-          lo = mid + 1;
-        } else {
+          break;
+        } else if (nums[mid] > target ) {
           hi = mid - 1;
+        } else {
+          lo = mid + 1;
         }
       }
-      return hi === -1 ? 0 : hi;
+      const mid = Math.floor((lo + hi) / 2);
+      const index = target <= nums[mid] ? mid - 1 : mid;
+      return Math.max(0, index);
     };
 
     const scrollHandler = () => {
